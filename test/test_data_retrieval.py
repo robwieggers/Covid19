c7d7b84b5cc80db5a1b2bd3c7f93b78c11deb74e
Rob Wieggers <rob.wieggers@asml.com>
2020-08-21 00:26:30 +0200

    cleanup around tests

--- test/test_data_retrieval.py
+++ test/test_data_retrieval.py
@@ -3,20 +3,18 @@ Test retrieval of files
 """
 
 import os
-import sys
-print(sys.path)
 
-from src.utils import get_data_path, get_rivm_filenames
+from src.utils import get_raw_data_path, get_rivm_filenames
 from src.ingest.load_data import retrieve_rivm_datasets
+
 
 def test01_rivm_data_retrieval():
 
     retrieve_rivm_datasets()
 
     # data path should exist
-    assert(os.path.exists(get_data_path()))
+    assert(os.path.exists(get_raw_data_path()))
 
     # data path should contain rivm datasets
     expected_filenames = list(get_rivm_filenames().values())
-    assert(all(elem in os.listdir(get_data_path()) for elem in expected_filenames))
-
+    assert(all(elem in os.listdir(get_raw_data_path()) for elem in expected_filenames))
